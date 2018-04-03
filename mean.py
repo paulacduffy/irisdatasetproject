a1d8683f9728144792440fc2e0b619a9feef23df
paulacduffy <G00352482@gmit.ie>
2018-04-03 20:10:03 +0100

    Used pandas to import dataset & set column names

---
+++ mean.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+#Import pandas & numpy libraries
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pd.read_csv(url, names=names)
+print (dataset)

0fefde5fd5958dfa4147ccffe33d51d9e30078e7
paulacduffy <G00352482@gmit.ie>
2018-04-03 20:11:30 +0100

    Added reference

--- mean.py
+++ mean.py
@@ -6,3 +6,5 @@ url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pd.read_csv(url, names=names)
 print (dataset)
+
+#Reference https://stackoverflow.com/questions/44594249/cannot-import-data-in-python-using-pandas for help loading dataset using pandas

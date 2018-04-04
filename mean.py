840e61052e211d96cdf19982a403fd7c1a85c118
paulacduffy <G00352482@gmit.ie>
2018-04-04 19:55:29 +0100

    Calculated summary statistics for each column

--- mean.py
+++ mean.py
@@ -5,6 +5,7 @@ import numpy as np
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pd.read_csv(url, names=names)
-print (dataset)
+print (dataset.describe())
 
 #Reference https://stackoverflow.com/questions/44594249/cannot-import-data-in-python-using-pandas for help loading dataset using pandas
+#Prints mean, standard deviation, minimum value, 25th percentile, 50th percentile, 75th percentile & maximum values for each column

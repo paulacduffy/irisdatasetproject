9e373cf029488b9051bf18b174d7563f6c901e1a
paulacduffy <G00352482@gmit.ie>
2018-04-04 20:25:23 +0100

    Dataset dimensions & no. of rows of each class

--- mean.py
+++ mean.py
@@ -9,3 +9,9 @@ print (dataset.describe())
 
 #Reference https://stackoverflow.com/questions/44594249/cannot-import-data-in-python-using-pandas for help loading dataset using pandas
 #Prints mean, standard deviation, minimum value, 25th percentile, 50th percentile, 75th percentile & maximum values for each column
+
+print (dataset.shape)
+#dimensions of the dataset
+
+print (dataset.groupby('class').size())
+#Number of rows of data for each class

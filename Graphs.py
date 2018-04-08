5a32babe55956c8a1c4fa06afc1818a2fce1e44b
paulacduffy <G00352482@gmit.ie>
2018-04-08 17:05:11 +0100

    Moved graphs to new file

---
+++ Graphs.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#Import pandas & numpy libraries
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pd.read_csv(url, names=names)
+
+dataset.hist
+plt.show()
+#Histograms of each column
+#Reference https://medium.com/codebagng/basic-an
+
+dataset.boxplot (by = "class")
+plt.show()
+#Boxplot of each column by Class

05c39962af77d39d1022533fcaf5e35f2aef7103
paulacduffy <G00352482@gmit.ie>
2018-04-12 20:03:40 +0100

    Amended layout

--- plots.py
+++ plots.py
@@ -1,3 +1,7 @@
+#Paula Duffy
+#Iris Dataset
+#Histogram & Boxplot
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,12 +11,13 @@ url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pd.read_csv(url, names=names)
 
-dataset.hist
-plt.show()
 #Histograms of each column
 #Reference https://medium.com/codebagng/basic-an
 
-dataset.boxplot (by = "class")
+dataset.hist
 plt.show()
-#Boxplot of each column by Class
 
+#Boxplot of each column by Class
+dataset.boxplot (by = "class")
+
+plt.show()

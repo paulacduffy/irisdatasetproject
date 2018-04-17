1cf67d0b23fa3affe77bda661faf85a8629a0c55
paulacduffy <G00352482@gmit.ie>
2018-04-17 20:28:16 +0100

    Fixed script

--- scatterplot.py
+++ scatterplot.py
@@ -7,6 +7,6 @@ url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pd.read_csv(url, names=names)
 
-#Scatterplot of all pairs of attributes
-plt.scatter (dataset)
+#Scatterplot of sepal length & sepal width
+dataset.plot(kind="scatter", x="sepal-length", y="sepal-width")
 plt.show()

c752b7b4f38092a0ceef458c8654c9f7d089b73d
paulacduffy <G00352482@gmit.ie>
2018-04-18 20:14:58 +0100

    Scatterplot with colour

--- scatterplot.py
+++ scatterplot.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="white", color_codes=True)
 #Import pandas, numpy & matplotlib libraries
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pd.read_csv(url, names=names)
 
-#Scatterplot of sepal length & sepal width
-dataset.plot(kind="scatter", x="sepal-length", y="sepal-width")
+
+#Scatterplot using Seaborn
+sns.lmplot(x="sepal-width", y="sepal-length", hue="class", data=dataset)
 plt.show()

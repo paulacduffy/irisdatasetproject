ff8914cb59a9a326e410319c248b4a0b6dc312b5
paulacduffy <G00352482@gmit.ie>
2018-04-27 13:58:25 +0100

    Added scatterplots

--- scatterplot.py
+++ scatterplot.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style="white", color_codes=True)
-#Import pandas, numpy & matplotlib libraries
+#Import pandas, numpy, matplotlib & seaborn libraries
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
@@ -16,6 +16,12 @@ plt.legend(loc = 'lower right')
 
 sns.lmplot(x="petal-width", y="petal-length", hue="class", data=dataset, legend_out = False)
 plt.legend(loc = 'lower right')
+
+sns.lmplot(x="sepal-length", y="petal-length", hue="class", data=dataset, legend_out = False)
+plt.legend(loc = 'upper left')
+
+sns.lmplot(x="sepal-width", y="petal-width", hue="class", data=dataset, legend_out = False)
+plt.legend(loc = 'upper left')
 
 plt.show()
 #reference: https://seaborn.pydata.org/examples/multiple_regression.html
